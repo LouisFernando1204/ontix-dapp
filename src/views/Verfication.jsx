11c07a1f090751d4e7df7274f85f8182f0519183
Richie Reuben Hermanto <richiereubenh@Richies-MacBook-Pro.local>
2025-05-30 00:08:09 +0700

    test

--- src/views/Verfication.jsx
+++ src/views/Verfication.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Verfication = () => {
     return (
         <div className="flex flex-col justify-center items-center h-full w-full text-pink-700">
-            Verification Page!
+            Verification Page! halo
         </div>)
 }
 
